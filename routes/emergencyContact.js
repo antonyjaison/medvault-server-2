680ceb18062f06f2c6e6ee9999460b7aa5205a76
Antony Jaison <antonyjaison639@gmail.com>
2024-06-19 12:22:58 +0530

    configured anthropic api

--- routes/emergencyContact.js
+++ routes/emergencyContact.js
@@ -1,14 +1,59 @@
 import { Router } from 'express'
-
+import twilio from 'twilio'
 const emergencyContactRouter = Router()
 
 
 emergencyContactRouter.post('/send-emergency', async (req, res) => {
     console.log('Request received')
 
-    const { location, emergencyContacts } = req.body
+    const { userDetails, emergencyContacts } = req.body
 
-    console.log(location, emergencyContacts)
+    console.log(emergencyContacts)
+
+    const name = userDetails.name
+    const longitude = userDetails.location.coords.longitude
+    const latitude = userDetails.location.coords.latitude
+    const mapUrl = `https://www.google.com/maps?q=${latitude},${longitude}`
+    const alertMessage = `
+    🚨 Emergency Alert from MedVault 🚨
+    
+    Dear Emergency Contact,
+    
+    This is an automated alert regarding ${name}. They have activated an emergency SOS signal indicating they may need immediate assistance.
+    
+    Current Location:
+    ${mapUrl}
+    
+    Latitude: ${latitude}
+    Longitude: ${longitude}
+    
+    Please contact them as soon as possible to ensure their safety. If you are unable to reach them, consider notifying local emergency services and provide them with the location details above.
+    
+    Thank you for your prompt attention to this urgent matter.
+    
+    Sincerely,
+    MedVault Emergency Alert System
+    `;
+
+    const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH)
+
+    emergencyContacts.forEach(async (contact) => {
+        try {
+            await client.messages.create({
+                body: alertMessage,
+                from: process.env.TWILIO_PHONE_NUMBER,
+                to: '+91' + contact.phone
+            }).then(() => {
+                client.calls.create({
+                    twiml: `<Response><Say voice="alice">Emergency! I am in danger. My location is: ${'Angamaly'}</Say></Response>`,
+                    to: '+91' + contact.phone,
+                    from: process.env.TWILIO_PHONE_NUMBER
+                })
+            })
+        } catch (error) {
+            console.error('Error sending message:', error)
+        }
+    })
 
 })
 export default emergencyContactRouter
--- routes/prescriptionRouter.js
+++ routes/prescriptionRouter.js
@@ -1,111 +1,122 @@
 import { Router } from 'express';
 import Anthropic from "@anthropic-ai/sdk";
 import getImageBase64 from '../utils/base64.js';
-import { GoogleGenerativeAI, HarmCategory } from "@google/generative-ai";
-import uploadToGemini from '../utils/uploadToGemini.js';
-
 
 const prescriptionRouter = Router();
 
-// prescriptionRouter.get('/prescription-data', async (req, res) => {
-//     const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/medvault-1ca13.appspot.com/o/prescription%2F1714508824229_IMG-20240430-WA0002.jpg?alt=media&token=2ff20503-1dc0-4f07-9721-c3ab6a309a3e';
-//     console.log('Request received');
-
-//     try {
-//         const base64Image = await getImageBase64(imageUrl);
-
-//         console.log(base64Image)
-
-
-//         const anthropic = new Anthropic({
-//             apiKey: process.env.ANTHROPIC_API_KEY, // defaults to process.env["ANTHROPIC_API_KEY"]
-//         });
-
-//         const msg = await anthropic.messages.create({
-//             model: "claude-3-opus-20240229",
-//             max_tokens: 1000,
-//             temperature: 0,
-//             messages: [
-//                 {
-//                     "role": "user",
-//                     "content": [
-//                         {
-//                             "type": "text",
-//                             "text": "from the image return a json file of list of pills along with how many times it should be taken and any other remark as an object; name:str, dose:str, frequency:number, remarks:long natural language str; return json data only without language marking. I need the data as array of objects don't use any formatting methods, you can also give me in the format that the array is stringity, then I can parse the array"
-//                         },
-//                         {
-//                             "type": "image",
-//                             "source": {
-//                                 "type": "base64",
-//                                 "media_type": "image/jpeg",
-//                                 "data": base64Image
-//                             }
-//                         }
-//                     ]
-//                 }
-//             ]
-//         });
-//         console.log(msg);
-
-
-//     } catch (error) {
-//         console.error('Error processing request:', error);
-//         res.status(500).send('An error occurred while processing your request.');
-//     }
-// });
-
-
-prescriptionRouter.get('/prescription-data', async (req, res) => {
-    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/medvault-1ca13.appspot.com/o/prescription%2F1714508824229_IMG-20240430-WA0002.jpg?alt=media&token=2ff20503-1dc0-4f07-9721-c3ab6a309a3e';
+prescriptionRouter.post('/prescription-data', async (req, res) => {
     console.log('Request received');
 
-    const genAI = new GoogleGenerativeAI('AIzaSyB9eTEl-szHeULqqhV2yFqyoKMOUvCiW34');
-
-    const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
-    });
-
-    const generationConfig = {
-        temperature: 2,
-        topP: 0.95,
-        topK: 64,
-        maxOutputTokens: 1024,
-        responseMimeType: "text/plain",
-    };
-
-    const files = [
-        await uploadToGemini("prescription.png", "image/png"),
-    ];
+    // const { image } = req.body
+    // console.log(image);
+    const image = 'https://firebasestorage.googleapis.com/v0/b/medvault-1ca13.appspot.com/o/prescription%2F1718704249960_IMG-20240618-WA0042.jpg?alt=media&token=d6710e5b-f1c6-41b2-b73b-3623299906e4'
 
     try {
-        const chatSession = model.startChat({
-            generationConfig,
-            history: [
-                {
-                    role: "user",
-                    parts: [
-                        {
-                            fileData: {
-                                mimeType: files[0].mimeType,
-                                fileUri: files[0].uri,
-                            },
-                        },
-                        { text: "analyse this image" },
-                    ],
-                },
-            ],
-        });
+            const base64Image = await getImageBase64(image);
 
-        const result = await chatSession.sendMessage("analyse this image");
-        console.log(result.response.text());
-        res.status(200).send(result.response.text());
+            const anthropic = new Anthropic({
+                apiKey: process.env.ANTHROPIC_API_KEY,
+            });
+
+            const msg = await anthropic.messages.create({
+                model: "claude-3-opus-20240229",
+                max_tokens: 1000,
+                temperature: 0,
+                messages: [
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": "from the image return a json file of list of pills along with how many times it should be taken and any other remark as an object; name:str, dose:str, frequency:number, remarks:long natural language str; return json data only without language marking. I need the data as array of objects don't use any formatting methods, you can also give me in the format that the array is stringity, then I can parse the array"
+                            },
+                            {
+                                "type": "image",
+                                "source": {
+                                    "type": "base64",
+                                    "media_type": "image/jpeg",
+                                    "data": base64Image
+                                }
+                            }
+                        ]
+                    }
+                ]
+            });
+            console.log(msg.content[0].text);
+            res.status(200).json(msg.content[0].text);
 
     } catch (error) {
         console.error('Error processing request:', error);
         res.status(500).send('An error occurred while processing your request.');
     }
-
-
 });
+
+
+// prescriptionRouter.get('/prescription-data', async (req, res) => {
+//     console.log('Request received');
+
+//     const imageUrl = 'https://media.tumblr.com/fa1b2e9d2e5464e495f81ca83da88533/tumblr_inline_mle2yerIZy1qz4rgp.jpg'
+
+//     const MODEL_NAME = "gemini-1.0-pro-vision-latest";
+//     const API_KEY = "AIzaSyDFhnm1R2_HY9o6YCNkrroNoUSuq-0mT5o";
+
+//     const response = await axios.get(imageUrl, {
+//         responseType: 'arraybuffer'
+//     });
+
+//     const base64Image = Buffer.from(response.data, 'binary').toString('base64');
+
+//     const genAI = new GoogleGenerativeAI(API_KEY);
+//     const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+//     const generationConfig = {
+//         temperature: 0.4,
+//         topK: 32,
+//         topP: 1,
+//         maxOutputTokens: 4096,
+//     };
+//     const safetySettings = [
+//         {
+//             category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+//             threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+//         },
+//         {
+//             category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+//             threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+//         },
+//         {
+//             category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+//             threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+//         },
+//         {
+//             category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+//             threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+//         },
+//     ];
+
+//     const parts = [
+//         { text: "YOU CAN REPLY IN A DETAILED RESPONSE ONLY. SEND ME A SHORT EXPLANATION OF THE LECTURE NOTE OF THE IMAGE I SEND YOU. \n\n" },
+//         { inlineData: { mimeType: "image/jpg", data: base64Image } },
+//     ];
+
+//     try {
+//         const result = await model.generateContent({
+//             contents: [{ role: "user", parts }],
+//             generationConfig,
+//             safetySettings,
+//         });
+
+//         const response = result.response;
+//         console.log(response.text());
+//         res.send(response.text());
+
+//     } catch (error) {
+//         console.error('Error processing request:', error);
+//         res.status(500).send('An error occurred while processing your request.');
+//     }
+
+
+
+// });
 
 export default prescriptionRouter;
